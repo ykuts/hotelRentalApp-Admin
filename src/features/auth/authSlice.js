eb37f9039679d6a8126b0a71b5b9d2d48033bca4
nabya47 <nabyanaseem47@gmail.com>
2025-04-07 16:48:12 +0500

    Update authSlice.js

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-const API_URL = process.env.REACT_APP_API_URL || "hotelrentalappserver-production.up.railway.app/api";
+export const API_URL = process.env.REACT_APP_API_URL || "hotelrentalappserver-production.up.railway.app/api";
 
 const user = JSON.parse(localStorage.getItem("user"));
 
