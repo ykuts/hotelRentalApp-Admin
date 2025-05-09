5d441500414d25ab4f1a58672a5570b1f6e425bd
yuliiak <98550411+ykuts@users.noreply.github.com>
2025-05-09 12:51:11 +0200

    Update EditRoom.jsx

--- src/Pages/EditRoom/EditRoom.jsx
+++ src/Pages/EditRoom/EditRoom.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { updateRoom, reset } from "../../features/room/roomSlice";
 import { useSelector, useDispatch } from "react-redux";
 
-export const API_URL = process.env.REACT_APP_API_URL || "https://hotel-rental-app-server.vercel.app";
+export const API_URL = process.env.REACT_APP_API_URL || "https://hotelrentalappserver.up.railway.app";
 
 const EditRoom = () => {
   const dispatch = useDispatch();
@@ -22,7 +22,7 @@ const EditRoom = () => {
   useEffect(() => {
     const getRoom = async () => {
       try {
-        const res = await fetch(`${API_URL}/rooms/${id}`);
+        const res = await fetch(`${API_URL}/api/rooms/${id}`);
         const data = await res.json();
 
         const { roomNumbers, ...rest } = data;
